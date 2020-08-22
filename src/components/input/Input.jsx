69d7cb6e053ea683caf2880eb976b28dffaea2a3
MattG-ZA <gleedmatt@gmail.com>
2020-08-22 19:31:41 +0200

    Added music and some other styling updates

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './Input.css'
-import ausFlag from '../../resources/australia-flag-square-icon-64.png'
-import gbFlag from '../../resources/united-kingdom-flag-square-icon-64.png'
+import ausFlag from '../../resources/ausFlag.png'
+import gbFlag from '../../resources/ukFlag.png'
+import ausAnthem from '../../resources/ausAnthem.mp3'
 
 const Input = (props) => {
     const { changeBackground } = props
@@ -9,18 +10,28 @@ const Input = (props) => {
     const [flag, setFlag] = useState(gbFlag)
     const [flip, setFlip] = useState(true)
     const [placeholder, setPlaceholder] = useState('Type some English here!')
+    const [audioEl, setAudioEl] = useState(new Audio(ausAnthem))
     const inputEl = useRef(null)
     const imgEl = useRef(null)
 
+    useEffect(() => {
+        audioEl.loop = true
+        setAudioEl(audioEl)
+    }, [audioEl])
+
     const changeLanguage = () => {
         setFlip(!flip)
+        changeBackground(flip)
+
         inputEl.current.style.setProperty('--transform', `rotateX(${flip ? 180 : 0}deg)`)
         imgEl.current.style.setProperty('--transform', `rotateX(${flip ? 180 : 0}deg)`)
+
         setTimeout(() => {
             setPlaceholder(flip ? 'Type some Australian here!' : 'Type some English here!')
             setFlag(flip ? ausFlag : gbFlag)
         }, 200)
-        changeBackground(flip)
+
+        flip ? audioEl.play() : audioEl.pause()
     }
 
     return (<span>
